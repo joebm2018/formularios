04732301361738cedb54e25f16168e36ac3f43ba
joebm2018 <volado23@gmail.com>
2019-10-11 17:09:36 -0500

    agregado sweetalert

--- src/app/components/crear-factura/crear-factura.component.ts
+++ src/app/components/crear-factura/crear-factura.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit , OnDestroy} from '@angular/core';
 import { FacturasService } from 'src/app/services/facturas.service';
 import {Subscription} from 'rxjs';
+import Swal from 'sweetalert2';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-crear-factura',
@@ -17,16 +19,29 @@ export class CrearFacturaComponent implements OnInit {
     fact_fech:''
   }
   subscriptor:Subscription;
-  constructor( private _sFactura:FacturasService) { }
+  constructor( private _sFactura:FacturasService,
+    private _sRouter:Router) { }
 
   ngOnInit() {
    
   }
+  sweetAlertGuardo(){
+    Swal.fire({
+      title: 'Excelente!',
+      text: 'se guardaron los datos correctamente',
+      type: 'success',
+      confirmButtonText: 'Aceptar'
+    }).then((result) => {
+      this._sRouter.navigate(['facturas']);
+    })
+  } 
   crearFactura(){
     console.log(this.objFactura);
     this.subscriptor=this._sFactura.postFactura(this.objFactura).subscribe((rpta)=>{
       console.log(rpta);
     });
+    this.sweetAlertGuardo();
+    //this._sRouter.navigate(['facturas']);
   }
   ngOnDestroy(): void {
     //Called once, before the instance is destroyed.
@@ -36,7 +51,7 @@ export class CrearFacturaComponent implements OnInit {
     } catch (error) {
       
     }
-    
   }
+  
 
 }
